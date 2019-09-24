581a48850fceedcf0877fb8f380d8958f52b8ee6
sidpremkumar <sid.premkumar@gmail.com>
2019-09-24 14:04:14 -0400

    Removing old labs

--- cs112/labs/lab10/getSumSolution.java
+++
@@ -1,9 +0,0 @@
-private T _getSum(Node<T> n)
-		{
-			 T ret = n.data;
-			 // Could use a basecase fo n.children.size()==0 and just return n.data, but not needed. 
-			 for(Node<T> child : n.children) {
-				 ret = ret.plus(_getSum(child));
-			 }
-			 return ret;
-		}
